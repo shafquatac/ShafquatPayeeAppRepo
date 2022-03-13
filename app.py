654f6e9ddcb79a1520cad691767f7664b321bcd8
shafquat <shafquatac@yahoo.com>
2022-03-13 13:15:40 +1300

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
 
 
 if __name__ == '__main__':
